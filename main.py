12c8e22704335ce2abf78235b48cbf15984a036e
RADIUMz-Codes <theabhikr03@gmail.com>
2024-04-04 12:25:08 +0530

    add jd rote

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI, UploadFile, File
 import shutil
-
+from fastapi.middleware.cors import CORSMiddleware
 # Create a FastAPI application
 app = FastAPI()
 
+# app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Define a route at the root web address ("/")
 @app.get("/")
 def read_root():
 	return {"message": "Hello, FastAPI!"}
 
+@app.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    with open(f"./uploads/cv/{file.filename}", "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+    return {"filename": file.filename}
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     with open(f"./uploads/cv/{file.filename}", "wb") as buffer:
